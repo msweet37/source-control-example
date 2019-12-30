a3f5e08e412bd8f60a053ba64729725887c3b520
Michael Sweet <msweet37@gmail.com>
2019-12-30 10:22:04 -0500

    Initial Commit

---
+++ Source Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control
+//
+//  Created by Michael Sweet on 12/30/19.
+//  Copyright © 2019 Michael Sweet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Michael Sweet on 12/30/19.
+//  Copyright © 2019 Michael Sweet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
